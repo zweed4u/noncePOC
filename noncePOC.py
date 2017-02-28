30fdf672b3aa6d6c8053dc89abf49c9e624f3c0f
Zack <zackweeden@aol.com>
2017-02-28 00:52:00 -0500

    Displaying proper output of command

--- noncePOC.py
+++ noncePOC.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 # make use of config - ssh credentials and path to blob and if nonceEnabler is needed
 # dependency check/install tihmstar stuff 
 import os, sys, time, datetime, requests, paramiko, ConfigParser
 from scp import SCPClient
+
+print 'Please ensure that your iDevice is in a jailbroken state and has tfp0 support.'
+print '***If you are unsure - please visit jbme.qwertyoruiop.com on your device before proceeding***'
+raw_input('')
 
 # Get the project directory to avoid using relative paths
 rootDirectory = os.getcwd()
@@ -32,25 +37,33 @@ class SSH:
 	def connect(self):
 		SSH.ssh.connect(self.address, self.port, self.user, self.passwd)
 
+print str(datetime.datetime.now())+' :: Loading config information...'
 user_config = Config()
+
+if user_config.binNeeded.lower() == 'false':
+	print 'Ensure that the binary is titled "nonceEnabler" and in /var/root'
+	raw_input('')
+
 ios_ssh = SSH(user_config.iosIp, 22, 'root', user_config.iosSshPass)
 local_ssh = SSH('127.0.0.1', 22, os.getlogin(), user_config.localPass)
 
-def nvramWrite(iOSSession, binaryFileName, generator):
-	print datetime.datetime.now()+' :: Setting nonceEnabler permissions...'
+def nvramWrite(iOSSession, generator, binaryFileName=None):
+	if binaryFileName == None:
+		binaryFileName = 'nonceEnabler'
+	print str(datetime.datetime.now())+' :: Setting nonceEnabler permissions...'
 	stdin, stdout, stderr = iOSSession.ssh.exec_command('chmod +x '+binaryFileName)
-	for i in stdout.readlines():
-		print i
-	print datetime.datetime.now()+' :: Running nonceEnabler...'
+
+	print str(datetime.datetime.now())+' :: Running nonceEnabler...'
 	stdin, stdout, stderr = iOSSession.ssh.exec_command('./'+binaryFileName)
-	for i in stdout.readlines():
-		print i
-	print datetime.datetime.now()+' :: Setting nvram generator variable for boot-nonce...'
+	print str(stdout.read()) #change color to indicate this is the console of ios
+
+	print str(datetime.datetime.now())+' :: Setting nvram generator variable for boot-nonce...'
 	iOSSession.ssh.exec_command('nvram com.apple.System.boot-nonce='+generator)
-	print datetime.datetime.now()+' :: Ensuring nvram variable was written...'
+
+	print str(datetime.datetime.now())+' :: Ensuring nvram variable was written...'
 	stdin, stdout, stderr = iOSSession.ssh.exec_command('nvram -p') #maybe grep this with the generator or com.apple.System.boot-nonce
 	for i in stdout.readlines():
-		print i # assert that the variable written matches the generator
+		print i, # assert that the variable written matches the generator - change color to indicate this is the console of ios
 
 for line in open(user_config.blobPath, 'r'):
 	if '<string>0x' in line: # ensure that each generator follows the same pattern (hex 0x and xml-esque <>__<>)
@@ -59,37 +72,38 @@ for line in open(user_config.blobPath, 'r'):
 if user_config.binNeeded.lower() == 'true':
 	fetchBinSession = requests.session()
 	binUrl = 'https://people.rit.edu/zdw7287/files/nonceEnabler/nonceEnabler'
-	print datetime.datetime.now()+' :: Downloading nonceEnabler binary...'
+	print str(datetime.datetime.now())+' :: Downloading nonceEnabler binary...'
 	r = fetchBinSession.get(binUrl)
 	local_filename = binUrl.split('/')[-1]
 	with open(local_filename, 'wb') as f:
 		for chunk in r.iter_content(chunk_size=1024): 
 			if chunk: # filter out keep-alive new chunks
 				f.write(chunk)
-	print datetime.datetime.now()+' :: SSHing into device using config vals...'
+
+	print str(datetime.datetime.now())+' :: SSHing into device using config vals...'
 	ios_ssh.connect()
-	print datetime.datetime.now()+' :: SCPing downloaded binary to device...'
+
+	print str(datetime.datetime.now())+' :: SCPing downloaded binary to device...'
 	scp = SCPClient(local_ssh.ssh.get_transport())
 	scp.put(os.path.join(rootDirectory, local_filename))
-	nvramWrite(ios_ssh, local_filename, generator)
+
+	nvramWrite(iOSSession=ios_ssh, generator=generator, binaryFileName=local_filename)
 
 if user_config.runEnabler.lower() == 'true':
-	print datetime.datetime.now()+' :: SSHing into device using config vals...'
+	print str(datetime.datetime.now())+' :: SSHing into device using config vals...'
 	ios_ssh.connect()
-	nvramWrite(ios_ssh, local_filename, generator)
+
+	nvramWrite(iOSSession=ios_ssh, generator=generator)
 
 # need to terminate ios ssh session
 if user_config.poc.lower() == 'true':
 	local_ssh.connect()
 	bnchNonce = str(local_ssh.ssh.exec_command('img4tool -s '+user_config.blobPath+' | grep BNCH')).split('BNCH: ')[2] #Format - BNCH: BNCH: (NONCE HERE)
-	print datetime.datetime.now()+' :: Connect your idevice in its jailbroken state - nonce should already have been set by enabler'
-	print datetime.datetime.now()+' :: Press Enter when device is connected.'
+	print str(datetime.datetime.now())+' :: Connect your idevice in its jailbroken state - nonce should already have been set by enabler'
+	print str(datetime.datetime.now())+' :: Press Enter when device is connected.'
 	raw_input('')
 	deviceNonce = str(local_ssh.ssh.exec_command('sudo noncestatistics -t 1 test.txt | grep ApNonce')).split('=')[1] #log in as root or password will be needed
 	assert deviceNonce == bnchNonce, "Error Message: Nonces do not match!"
-
-print 'Ensure that you are currently in a jailbroken state and that you have the blobs.'
-print 'Visit jbme.qwertyoruiop.com on your device for tfp0'
 
 # fetch path to blob from config and parse and store generator
 # fetch ssh credentials from config
